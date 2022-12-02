a55b8ec48282220d1f9c82d4e839904740d372ec
bomin-1206 <gbm4655@naver.com>
2022-12-03 01:47:02 +0900

    feat/writepage 생성

---
+++ src/routes/WritePage/index.jsx
@@ -0,0 +1,22 @@
+import styles from './write.module.scss'
+
+function Writepage() {
+    return (
+        <div className='writepage'>
+            {
+
+            }
+        </div>
+    )
+}
+
+function showwrite() {
+    return (
+        <div className='showwirte'>
+            <h4>제목</h4>
+
+        </div>
+    )
+}
+
+export default Writepage;
