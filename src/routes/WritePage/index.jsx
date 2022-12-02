669e5814442c4dc9e36eee6e31b77ecd509c6891
bomin-1206 <gbm4655@naver.com>
2022-12-03 02:04:54 +0900

    index.jsx

--- src/routes/WritePage/index.jsx
+++ src/routes/WritePage/index.jsx
@@ -1,18 +1,3 @@
-<<<<<<< Updated upstream
-import styles from './write.module.scss'
+import styles from './wirte.module.scss';
 
-function Writepage() {
-  return <div className='writepage'>{}</div>
-}
-
-function showwrite() {
-  return (
-    <div className='showwirte'>
-      <h4>제목</h4>
-    </div>
-  )
-}
-
-export default Writepage
-=======
->>>>>>> Stashed changes
+function 
