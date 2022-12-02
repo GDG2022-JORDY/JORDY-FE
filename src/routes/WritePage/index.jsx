fafc39964b044b131b6285b2cb44624509b91d30
bomin-1206 <gbm4655@naver.com>
2022-12-03 02:27:04 +0900

    feat/writepage

--- src/routes/WritePage/index.jsx
+++ src/routes/WritePage/index.jsx
@@ -1,13 +1,47 @@
 import styles from './write.module.scss'
+import Table from 'react-bootstrap/Table'
 
 function Writepage() {
-  return <div className='writepage'>{}</div>
+  return <div className='writepage'>
+    {
+
+    }
+  </div>
 }
 
 function showwrite() {
   return (
     <div className='showwirte'>
       <h4>제목</h4>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan={2}>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
     </div>
   )
 }
