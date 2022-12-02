4e5e749934f980458768667c66205a8f1c01d649
bomin-1206 <gbm4655@naver.com>
2022-12-03 04:02:11 +0900

    feat/writepage css빼고 안성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import { LogoImage } from './assets/svgs'
 import './App.scss'
 
 function App() {
-  return (
-    <div className='app'>
-    
-    </div>
-  )
+  return <div className='app' />
 }
 
 export default App
--- src/routes/WritePage/index.jsx
+++ src/routes/WritePage/index.jsx
@@ -1,47 +1,83 @@
-import styles from './write.module.scss'
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import './write.module.scss'
 import Table from 'react-bootstrap/Table'
+import {useState} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { AiFillEnvironment, AiFillFire, AiFillCloud , AiFillFile} from 'react-icons/ai'
 
 function Writepage() {
+  const [num, setNum] = useState(0)
   return <div className='writepage'>
     {
-
+      num === 1 ? <Showwrite /> : <Write />
     }
   </div>
 }
 
-function showwrite() {
+function Showwrite() {
   return (
     <div className='showwirte'>
-      <h4>제목</h4>
+      <h4>~~</h4>
       <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
-          </tr>
-        </thead>
         <tbody>
           <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
+            <td>글쓴이</td>
+            <td>~~</td>
           </tr>
           <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td colSpan={2}>Larry the Bird</td>
-            <td>@twitter</td>
+            <td>글 작성일</td>
+            <td>~~</td>
           </tr>
         </tbody>
       </Table>
+      <Content />
+    </div>
+  )
+}
+
+function Content() {
+  const [content, setcontent] = useState(['장소', '종목', '날짜', '친밀도'])
+  return (
+    <div className='list'>
+      <div className='place'>
+        <AiFillEnvironment />
+        <p>{content[0]}</p>
+        <p>~~</p>
+      </div>
+      <div className='evnet'>
+        <AiFillCloud />
+        <p>{content[1]}</p>
+        <p>~~</p>
+      </div>
+      <div className='date'>
+        <AiFillFile />
+        <p>{content[2]}</p>
+        <p>~~</p>
+      </div>
+      <div className='love'>
+        <AiFillFire />
+        <p>{content[3]}</p>
+        <p>~~~</p>
+      </div>
+    </div>
+  )
+}
+
+function Write() {
+  return (
+    <div className='write'>
+      <label>제목</label>
+      <input type="text" />
+      <select>
+        <option>전체</option>
+        <option>축구</option>
+        <option>농구</option>
+        <option>야구</option>
+      </select>
+      <label>만날장소</label>
+      <input type="text" />
+      <label>만날날짜</label>
+      <input type="date" />
     </div>
   )
 }
