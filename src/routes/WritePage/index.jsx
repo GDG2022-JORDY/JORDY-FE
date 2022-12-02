b0bd9ac1544e839b30336a3ed24f4807b1cd614e
malgamlee <malgamlee@gmail.com>
2022-12-03 02:05:52 +0900

    fix : write page 충돌 해결

--- src/routes/WritePage/index.jsx
+++ src/routes/WritePage/index.jsx
@@ -1,3 +1,15 @@
-import styles from './wirte.module.scss';
+import styles from './write.module.scss'
 
-function 
+function Writepage() {
+  return <div className='writepage'>{}</div>
+}
+
+function showwrite() {
+  return (
+    <div className='showwirte'>
+      <h4>제목</h4>
+    </div>
+  )
+}
+
+export default Writepage
